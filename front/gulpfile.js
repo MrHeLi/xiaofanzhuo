ecb05aa18390bbf12b313d255fba2ff9006a6fb6
kiven <superliunique@gmail.com>
2020-04-14 12:22:59 +0800

    Initial commit

---
+++ front/gulpfile.js
@@ -0,0 +1,91 @@
+const gulp = require("gulp");
+const cssnano = require("gulp-cssnano");
+const rename = require("gulp-rename");
+const uglify = require("gulp-uglify");
+const concat = require("gulp-concat");
+const imagemin = require("gulp-imagemin");
+const cache = require("gulp-cache");
+const watch = require("gulp-watch");
+const sass = require("gulp-sass");
+const util = require("gulp-util");
+const sourcemaps = require("gulp-sourcemaps");
+const bs = require("browser-sync").create();
+
+const path = {
+    'html': './templates/**/',
+    'css': './src/css/**/',
+    'js': './src/js/',
+    'images': './src/images/',
+    'css_dist': './dist/css/',
+    'js_dist': './dist/js/',
+    'images_dist': './dist/images/'
+};
+
+gulp.task('html', function (done) {
+    gulp.src(path.html + '*.html')
+        .pipe(bs.stream());
+    done()
+});
+
+gulp.task('css', function (done) {
+    gulp.src(path.css + '*.scss')
+        .pipe(sass().on("error", sass.logError))
+        .pipe(cssnano())
+        .pipe(rename({'suffix': ".min"}))
+        .pipe(gulp.dest(path.css_dist))
+        .pipe(bs.stream());
+    done();
+});
+
+// gulp.task('js', function (done) {
+//     gulp.src(path.js + '*.js')
+//         .pipe(sourcemaps.init)
+//         .pipe(uglify({
+//             'toplevel': true
+//         }).on("error", util.log))
+//         .pipe(rename({'suffix': ".min"}))
+//         .pipe(sourcemaps.write())
+//         .pipe(gulp.dest(path.js_dist))
+//         .pipe(bs.stream());
+//     done();
+// });
+
+gulp.task('js', function (done) {
+    gulp.src(path.js + '*.js')
+        .pipe(uglify({
+            'toplevel': true
+        }))
+        .pipe(rename({'suffix': ".min"}))
+        .pipe(gulp.dest(path.js_dist))
+        .pipe(bs.stream());
+    done();
+});
+
+gulp.task('images', function (done) {
+    gulp.src(path.images + "*.*")
+        .pipe(cache(imagemin()))
+        .pipe(gulp.dest(path.images_dist))
+        .pipe(bs.stream());
+    done();
+});
+
+gulp.task('watch', function (done) {
+    watch(path.html, gulp.series('html'));
+    watch(path.css, gulp.series('css'));
+    watch(path.js, gulp.series('js'));
+    watch(path.images, gulp.series('images'));
+    done();
+});
+
+gulp.task("bs", function() {
+	bs.init({
+		'server': {
+			'baseDir': './'
+		}
+	})
+});
+
+gulp.task("default", gulp.parallel('bs', 'watch'));
+
+
+
---
+++ front/src/js/index.js
@@ -0,0 +1,130 @@
+// 原型链 prototype
+function Banner() { // 定义类
+    // 类似构造函数
+    this.bannerWidth = 798;
+    this.bannerGroup = $("#banner-group");
+    this.index = 1;
+    this.leftArrow = $('#left-arrow');
+    this.rightArrow = $('#right-arrow');
+    this.bannerUl = $('#banner-ul');
+    this.liList = this.bannerUl.children("li");
+    this.bannerCount = this.liList.length;
+    this.pageControl = $('.page-control');
+}
+
+Banner.prototype.initBanner = function () {
+    var self = this;
+    var firstBanner = self.liList.eq(0).clone();
+    var lastBanner = self.liList.eq(self.bannerCount - 1).clone();
+    self.bannerUl.append(firstBanner);
+    self.bannerUl.prepend(lastBanner);
+    self.bannerUl.css({"width": self.bannerWidth * (self.bannerCount + 2), "left": -self.bannerWidth})
+};
+
+Banner.prototype.initPageControl = function () {
+    var self = this;
+    for (var i = 0; i < self.bannerCount; i++) {
+        var circle = $("<li></li>")
+        self.pageControl.append(circle);
+        if (i === 0) {
+            circle.addClass("active");
+        }
+    }
+    self.pageControl.css({"width": 12 * self.bannerCount + 8 * 2 + 16 * (self.bannerCount - 1)})
+};
+
+Banner.prototype.listenBannerHomer = function () {
+    var self = this;
+    this.bannerGroup.hover(function () {
+        clearInterval(self.timer);
+        self.toggleArrow(true);
+    }, function () {
+        self.loop();
+        self.toggleArrow(false);
+    });
+};
+
+Banner.prototype.loop = function () {
+    var self = this;
+    this.timer = setInterval(function () { // 定时器
+        self.index++;
+        if (self.index === self.bannerCount + 2) {
+            self.bannerUl.css({"left": -self.bannerWidth});
+            self.index = 2;
+        }
+        self.animate();
+    }, 2000);
+};
+
+Banner.prototype.toggleArrow = function (isShow) {
+    var self = this;
+    if (isShow) {
+        self.leftArrow.show();
+        self.rightArrow.show();
+    } else {
+        self.leftArrow.hide();
+        self.rightArrow.hide();
+    }
+};
+
+Banner.prototype.listenArrowClick = function () {
+    console.log("listenArrowClick");
+    var self = this;
+    self.leftArrow.click(function () {
+        self.index--;
+        if (self.index < 0) {
+            self.bannerUl.css({"left": -self.bannerWidth * self.bannerCount});
+            self.index = self.bannerCount - 1;
+        }
+        self.animate();
+    });
+
+    self.rightArrow.click(function () {
+        self.index++;
+        if (self.index >= self.bannerCount + 2) {
+            self.bannerUl.css({"left": -self.bannerWidth});
+            self.index = 2;
+        }
+        self.animate();
+    });
+};
+
+Banner.prototype.listenPageControl = function () {
+    var self = this;
+    // noinspection JSUnresolvedFunction
+    self.pageControl.children("li").each(function (index, obj) {
+        $(obj).click(function () {
+            self.index = index;
+            self.animate();
+        });
+    });
+};
+
+Banner.prototype.animate = function () {
+    var self = this;
+    this.bannerUl.animate({"left": -self.bannerWidth * self.index}, 500);
+    var index = self.index;
+    if (index === 0) {
+        index = self.bannerCount - 1;
+    } else if (index === self.bannerCount + 1) {
+        index = 0;
+    } else {
+        index = self.index - 1;
+    }
+    self.pageControl.children("li").eq(index).addClass("active").siblings().removeClass("active");
+};
+
+Banner.prototype.run = function () { // 给类定义函数
+    this.loop();
+    this.listenArrowClick();
+    this.listenBannerHomer();
+    this.initPageControl();
+    this.listenPageControl();
+    this.initBanner();
+};
+
+// $ jquery定义的，只有在文本元素加载完后才会执行
+$(function () {
+    var banner = new Banner(); // 创建类对象
+    banner.run();
+});
